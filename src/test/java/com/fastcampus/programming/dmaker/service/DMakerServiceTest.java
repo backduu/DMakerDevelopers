d65fe7d262132d0a433357e9cc99b50109200ca0
BackDoo <power4290@gmail.com>
2023-04-04 14:34:19 +0900

    #1. [테스트] Mockito를 이용한 Service 테스트 완료
    - SUCCESS일 때, FAILED일 때 비교하여 각각 테스트하는 방법 스터디

--- src/test/java/com/fastcampus/programming/dmaker/service/DMakerServiceTest.java
+++ src/test/java/com/fastcampus/programming/dmaker/service/DMakerServiceTest.java
@@ -5,25 +5,21 @@ import com.fastcampus.programming.dmaker.Repository.RetiredDeveloperRepository;
 import com.fastcampus.programming.dmaker.code.StatusCode;
 import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
 import com.fastcampus.programming.dmaker.dto.DeveloperDetailDto;
-import com.fastcampus.programming.dmaker.dto.DeveloperDto;
 import com.fastcampus.programming.dmaker.entity.Developer;
-import com.fastcampus.programming.dmaker.type.DeveloperLevel;
+import com.fastcampus.programming.dmaker.exception.DMakerErrorCode;
+import com.fastcampus.programming.dmaker.exception.DMakerException;
 import com.fastcampus.programming.dmaker.type.DeveloperSkillType;
-import com.fasterxml.jackson.databind.deser.std.StdKeyDeserializer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 import java.util.Optional;
 
-import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.time;
+import static com.fastcampus.programming.dmaker.type.DeveloperLevel.NEW;
+import static com.fastcampus.programming.dmaker.type.DeveloperSkillType.FRONT_END;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
@@ -39,25 +35,38 @@ class DMakerServiceTest {
 
     @InjectMocks
     private DMakerService dMakerService;
+    private final Developer defaultDeveloper = Developer.builder()
+            .developerLevel(NEW)
+            .developerSkillType(FRONT_END)
+            .experienceYears(1)
+            .name("test")
+            .sex("female")
+            .age(22)
+            .spec("1 project")
+            .statusCode(StatusCode.EMPLOYED)
+            .build();
+
+    private final CreateDeveloper.Request defaultCreateRequest = CreateDeveloper.Request.builder()
+            .developerLevel(NEW)
+            .developerSkillType(FRONT_END)
+            .experienceYears(1)
+            .name("test")
+            .sex("female")
+            .age(22)
+            .spec("1 project")
+            .memberId("memberId")
+            .build();
+
     @Test
     public void testSomething() {
+
         // 1. @Mock들의 동작을 정의
         given(developerRepository.findByMemberId(anyString()))
-                .willReturn(Optional.of(Developer.builder()
-                                .developerLevel(DeveloperLevel.NEW)
-                                .developerSkillType(DeveloperSkillType.FRONT_END)
-                                .experienceYears(1)
-                                .name("test")
-                                .sex("female")
-                                .age(22)
-                                .spec("1 project")
-                                .statusCode(StatusCode.EMPLOYED)
-                                .build()
-                ));
+                .willReturn(Optional.of(defaultDeveloper));
 
         DeveloperDetailDto developerDetailDto = dMakerService.getDeveloperDetail("memberId");
 
-        assertEquals(DeveloperLevel.NEW, developerDetailDto.getDeveloperLevel());
+        assertEquals(NEW, developerDetailDto.getDeveloperLevel());
         assertEquals(DeveloperSkillType.BACK_END, developerDetailDto.getDeveloperSkillType());
         assertEquals(2, developerDetailDto.getExperienceYears());
     }
@@ -65,29 +74,34 @@ class DMakerServiceTest {
     @Test
     void createDeveloperTest_success() {
         // given
-        CreateDeveloper.Request request = CreateDeveloper.Request.builder()
-                .developerLevel(DeveloperLevel.NEW)
-                .developerSkillType(DeveloperSkillType.FRONT_END)
-                .experienceYears(1)
-                .name("test")
-                .sex("female")
-                .age(22)
-                .spec("1 project")
-                .memberId("memberId")
-                .build();
-
         given(developerRepository.findByMemberId(anyString()))
                 .willReturn(Optional.empty());
 
         ArgumentCaptor<Developer> captor = ArgumentCaptor.forClass(Developer.class);
 
         // when
-        dMakerService.createDeveloper(request);
+        dMakerService.createDeveloper(defaultCreateRequest);
 
         // then
         // repository에 넘어갈때 save하는 파라미터를 캡처한다.
         verify(developerRepository, times(1)).save(captor.capture());
+        Developer saveDeveloper = captor.getValue();
+        assertEquals(NEW, saveDeveloper.getDeveloperLevel());
+        assertEquals(FRONT_END, saveDeveloper.getDeveloperSkillType());
+        assertEquals(1, saveDeveloper.getExperienceYears());
+    }
 
+    @Test
+    void createDeveloperTest_failed_with_duplicated() {
+        // given
+        given(developerRepository.findByMemberId(anyString()))
+                .willReturn(Optional.of(defaultDeveloper));
 
+        // when
+        // then
+        DMakerException dMakerException = assertThrows(DMakerException.class,
+                () -> dMakerService.createDeveloper(defaultCreateRequest));
+
+        assertEquals(DMakerErrorCode.DUPLICATED_MEMBER_ID, dMakerException.getDMakerErrorCode());
     }
 }
